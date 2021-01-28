f7d76f22d0c6045440a30bcc06dc938cec4c424e
robinsrk <robinsrk3@gmail.com>
2021-01-29 01:27:44 +0600

    updated some changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function() {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
-    document.getElementById('welcome').innerHTML = 'Your Account'
+    document.getElementById('welcome').innerHTML = 'Your Account';
+    document.getElementById('transaction-area').style.display = "block";
 })
-console.log("hello Rabby");
